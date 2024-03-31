938be7b7e3e80f243b3bcf1fa76cbfac852e7094
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-03-31 21:34:35 +0530

    made various components header footer button input logout logo, container

--- BlogAppAppwriteReact/src/components/index.js
+++ BlogAppAppwriteReact/src/components/index.js
@@ -1,4 +1,16 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
-export {Header,Footer}
+import Logo from "./Logo";
+import Container from "./Container/Container";
+import Button from "./Button";
+import Input from "./Input";
+import LogoutBtn from './Header/LogoutBtn'
+export {
+    Header,
+    Footer,
+    Logo,
+    Container,
+    Button,
+    Input,
+    LogoutBtn
+}
